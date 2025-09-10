9255e6f008e721a80366835df9d3a5972f3a4872
MumukshSaini <arndkmr01@gmail.com>
2025-09-10 20:55:46 +0530

    Add files via upload

---
+++ Untitled-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    float f;
+    char c;
+    double d;
+
+    printf("Enter a integer:")
+    scanf("%d",&i);
+
+    printf("Enter a float:")
+    scanf("%f",&f);
+
+    printf("Enter a character:")
+    scanf("%c",&c);
+
+    printf("Enter a double:")
+    scanf("%lf",&d);
+
+    printf("Integer:%d")
+}
